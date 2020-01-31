7f07b34ec7c877423d86b44b493d4eb3314b240c
furkanaydar <furkanaydar97@gmail.com>
2020-01-31 04:49:25 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withImages = require('next-images')
 module.exports = withImages({
   env: {
   //host : 'localhost:3000'
-    host: `furkanaydar.com`,
+    host: `www.furkanaydar.com`,
   },
   webpack: config => {
 
