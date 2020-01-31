0d792da15c2ba5ed9a52dc7355036b4c7cf91f31
furkanaydar <furkanaydar97@gmail.com>
2020-01-31 04:52:56 +0300

    rollback

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withImages = require('next-images')
 module.exports = withImages({
   env: {
   //host : 'localhost:3000'
-    host: `www.furkanaydar.com`,
+    host: `furkanaydar-blog.herokuapp.com`,
   },
   webpack: config => {
 
